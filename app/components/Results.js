a63760e930bee2ea11c9aaf1f107a26cdaf2b634
Ken Luo <luozhirong@gmail.com>
2017-05-31 18:56:50 -0700

    finish highly reusable components

--- app/components/Results.js
+++ app/components/Results.js
@@ -89,7 +89,7 @@ class Results extends React.Component {
         var loading = this.state.loading;
 
         if (loading === true) {
-            return <p>Loading</p>
+            return <Loading/>
         }
 
         if (error) {
